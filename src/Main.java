e8e06e10c122ff70eaa1b137197ae440d3e9004f
Anna test <annaterekhov1@gmail.com>
2022-12-27 21:30:45 +0200

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         int bonus = 1;
 
         int addBonus;
-        if (addBalance >= 1000) {
+        if (addBalance > 1000) {
             addBonus = (addBalance / 100 * bonus);
         } else {
             addBonus = 0;
         }
-        int amount = (balance + addBalance +addBonus);
+        int amount = (balance + addBalance + addBonus);
 
         System.out.println("Баланс:" + amount + " в том числе колличество бонусных рублей: " + addBonus);
     }
